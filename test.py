f6a6104d3532d0a76ebcf66c3d34220c94120d74
bigman911 <35257637+bigman911@users.noreply.github.com>
2018-07-23 18:08:22 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import time
-def countdown(n):
+def countdown(n) :
     while n > 0:
         print (n)
-    n = n - 1
-if n == 0:
-    print('Slade got you')
+        n = n - 1
+        if n == 0:
+            print('Slade got you')
 countdown(5)
-
-
