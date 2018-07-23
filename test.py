3052cf72287436c04abcc9bdf45ad91c62299dfd
bigman911 <35257637+bigman911@users.noreply.github.com>
2018-07-23 18:04:03 +0100

    timer indents

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import time
 def countdown(n) :
-while n > 0:
-print (n)
-n = n - 1
-if n ==0:
-print('Slade got you')
+    while n > 0:
+        print (n)
+    n = n - 1
+if n == 0:
+    print('Slade got you')
 countdown(5)
 
 
