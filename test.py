54fb564ee1cf80954ae5d61908683e31c8a179d7
bigman911 <35257637+bigman911@users.noreply.github.com>
2018-07-23 18:01:51 +0100

    adds timer

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import time
-
-
-count = 100
-
-start = input("Welcome to Reign of Terror \n 1-Start Game 2-Highscores")
-if start == "2":
-
-    hisc = open("highscore.txt", "a+")
-    hisc.write(str(count) + "\n")
+def countdown(n) :
+while n > 0:
+print (n)
+n = n - 1
+if n ==0:
+print('Slade got you')
+countdown(5)
 
 
