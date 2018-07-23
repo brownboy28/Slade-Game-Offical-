67ce62bbce5fbef23459f0ebbcbedc2ddc0b2098
brownboy28 <41588154+brownboy28@users.noreply.github.com>
2018-07-23 18:05:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import time
-def countdown(n):
-    while n > 0:
-        print (n)
-    n = n - 1
-if n == 0:
-    print('Slade got you')
+def countdown(n) :
+while n > 0:
+print (n)
+n = n - 1
+if n ==0:
+print('Slade got you')
 countdown(5)
 
+###############################################################################################################################################
 
+count = 0
+print("start pressing")
+while count < 30:
+  input("")
+  count = count + 1
+  print("number of pushes", count)
