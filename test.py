061e532d93b4051d20105206ee525d6cefd769b6
brownboy28 <41588154+brownboy28@users.noreply.github.com>
2018-07-23 18:11:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,12 @@ def countdown(n) :
         if n == 0:
             print('Slade got you')
 countdown(5)
+
+############################################
+
+count = 0
+print("start pressing")
+while count < 30:
+    input("")
+    count = count + 1
+    print("number of presses", count)
